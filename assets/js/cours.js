956bc04716b7351a823d1c4c229575979f7ee91f
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-06-21 10:26:59 +0200

    Ajout de la protection des saisies

--- assets/js/cours.js
+++ assets/js/cours.js
@@ -1,6 +1,12 @@
 function Number(){
-  var pointure= document.getElementById("shoeSize").value;
-  var annee= document.getElementById("yearOfBirth").value;
-  var resultat=((((pointure*2)+5)*50)-annee) + 1766;
-  alert(resultat);
+  var shoeSize = document.getElementById("shoeSize").value;
+  var yearOfBirth = document.getElementById("yearOfBirth").value;
+  var regex1 = /^[\d]{2}$/;
+  var regex2 = /^[\d]{4}$/;
+  var resultat =((((shoeSize*2)+5)*50)-yearOfBirth) + 1766;
+  if((regex1.test(shoeSize) == true) && (regex2.test(yearOfBirth) == true)){
+    alert(resultat);
+  }else{
+    alert("Vérifiez que vous avez saisie 2 chiffres pour la pointure et 4 chiffres pour l'année");
+  }
 }
